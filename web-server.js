56313125ae10e3f4e7e597b596528d98a1a1169a
DESKTOP-7S2NFG8\USER <amalharb@deakin.edu.au>
2020-10-15 18:05:38 +1100

    done

---
+++ web-server.js
@@ -0,0 +1,35 @@
+var express=require('express')
+var moment= require('moment')
+
+var app=express()
+
+
+// serve static content 'wep page'
+app.use(express.static(__dirname + '/public'));
+
+
+var log=function(message){
+    var time=moment().format()
+    console.log('[Server] @'+time+' '+message)
+
+}
+var adder=function(num1,num2){
+    var result = num1+num2;
+    return result
+} 
+
+/*
+app.get('/adder',function(req,res){
+    log('Adder Request Made')
+    var num1=parseInt(req.query.num1);
+    var num2=parseInt(req.query.num2);
+    var result = adder(num1,num2)
+    res.send(''+result+'')
+})
+ */
+
+
+
+var port=3000;
+app.listen(port)
+log('Server listening on: '+port)
